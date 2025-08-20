776200c8721b91124fe426d41b7bf5bc281dc77f
Anoushkha1225 <vedaanoushkha@gmail.com>
2025-08-20 09:50:33 +0530

    env added

--- backend/main.py
+++ backend/main.py
@@ -125,4 +125,4 @@ async def test_post(data: TestMessage):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="localhost", port=port)
